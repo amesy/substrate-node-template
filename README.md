9c34a6df079ec1140df12618619628400f72fa79
amesy <amesy1110@gmail.com>
2022-09-03 01:02:16 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# Substrate Node Template
+
+[![Try on playground](https://img.shields.io/badge/Playground-Node_Template-brightgreen?logo=Parity%20Substrate)](https://docs.substrate.io/playground/) [![Matrix](https://img.shields.io/matrix/substrate-technical:matrix.org)](https://matrix.to/#/#substrate-technical:matrix.org)
+
+> 原项目链接: https://github.com/substrate-developer-hub/substrate-node-template
+
+## Getting Started
+
+### Build
+
+```bash
+cargo build --release
+```
+
+### Run
+
+```bash
+./target/release/node-template --dev
+```
+
+### Test
+
+```bash
+cargo test -p  PALLETS-PACKAGE-NAME
+```
+
+## Substrate 区块链应用开发进阶
+
+### 第一次作业
+
+[代码执行截图](https://p.qlogo.cn/qqmail_head/C6nnRGnPbvwlVslNHxDtemvOjTjEDAZ1kU7KNglZeRibZgJNQnGzfofwQmCIllxjZhUk6ibxA2ianA/0)
---
+++ pallets/poe/src/tests.rs
@@ -0,0 +1,97 @@
+use crate::mock::*;
+use frame_support::{assert_ok, assert_noop};
+use sp_runtime::BoundedVec;
+use super::*;
+
+/// 创建存证
+#[test]
+fn create_claim_works() {
+	new_test_ext().execute_with(|| {
+		let claim = vec![0, 1];
+		assert_ok!(PoeModule::create_claim(Origin::signed(1), claim.clone()));
+
+		let bounded_claim = BoundedVec::<u8, <Test as Config>::MaxClaimLength>::try_from(claim.clone()).unwrap();
+		assert_eq!(Proofs::<Test>::get(&bounded_claim), Some((1, frame_system::Pallet::<Test>::block_number())));
+	})
+}
+
+/// 创建存证失败
+#[test]
+fn create_claim_failed_when_claim_already_exist() {
+	new_test_ext().execute_with(|| {
+		let claim = vec![0, 1];
+		
+		assert_ok!(PoeModule::create_claim(Origin::signed(1), claim.clone()));
+
+		assert_noop!(
+			PoeModule::create_claim(Origin::signed(1), claim.clone()),
+			Error::<Test>::ProofAlreadyExist
+		);
+	})
+}
+
+/// 撤销存证
+#[test]
+fn revoke_claim_works() {
+	new_test_ext().execute_with(|| {
+		let claim = vec![0, 1];
+		let bounded_claim = BoundedVec::<u8, <Test as Config>::MaxClaimLength>::try_from(claim.clone()).unwrap();
+
+		assert_ok!(PoeModule::create_claim(Origin::signed(1), claim.clone()));
+		assert_ok!(PoeModule::revoke_claim(Origin::signed(1), claim.clone()));
+
+		assert_eq!(Proofs::<Test>::get(&bounded_claim), None);
+	})
+}
+
+/// 撤销存证失败- 存证不存在
+#[test]
+fn revoke_claim_failed_when_claim_already_revoke() {
+	new_test_ext().execute_with(|| {
+		let claim = vec![0, 1];
+
+		assert_noop!(
+			PoeModule::revoke_claim(Origin::signed(1), claim.clone()),
+			Error::<Test>::ClaimNotExist
+		);
+	})
+}
+
+/// 撤销存证失败 - 撤销不属于自己的存证
+#[test]
+fn revoke_claim_failed_when_claim_not_have_permission() {
+	new_test_ext().execute_with(|| {
+		let claim = vec![0, 1];
+		assert_ok!(PoeModule::create_claim(Origin::signed(1), claim.clone()));
+
+		assert_noop!(
+			PoeModule::revoke_claim(Origin::signed(2), claim.clone()),
+			Error::<Test>::NotClaimOwner
+		);
+	})
+}
+
+/// 转移存证
+#[test]
+fn transfer_claim_works() {
+	new_test_ext().execute_with(|| {
+		let claim = vec![0, 1];
+
+		assert_ok!(PoeModule::create_claim(Origin::signed(1), claim.clone()));
+		assert_ok!(PoeModule::transfer_claim(Origin::signed(1), claim.clone(), 1));
+	})
+}
+
+
+/// 转移存证失败
+#[test]
+fn transfer_claim_failed_when_claim_already_transfer() {
+	new_test_ext().execute_with(|| {
+		let claim = vec![0, 1];
+
+		assert_noop!(
+			PoeModule::transfer_claim(Origin::signed(1), claim.clone(), 1),
+			Error::<Test>::ClaimNotExist
+		);
+	})
+}
